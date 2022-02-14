1761f6bea5382e5ea98483a4d869d58392165783
AbdouSamad99 <95620804+AbdouSamad99@users.noreply.github.com>
2022-02-14 18:02:13 +0100

    Create 10.py

---
+++ Phyton/PLS FIX/10.py
@@ -0,0 +1,13 @@
+'''
+The output should be:
+the number is 20
+'''
+print('the number is' + 20) #you can not combine a Str with a Int (number), Change the number into a Str
+
+
+#Fixed:
+
+print('the number is' + " 20")
+
+
+
