66a5ab54cc547d60670de38dd8207811ab60d883
AbdouSamad99 <95620804+AbdouSamad99@users.noreply.github.com>
2022-02-14 17:04:05 +0100

    Create 3.py

---
+++ Phyton/PLS FIX/3.py
@@ -0,0 +1,18 @@
+'''
+The output should be:
+30
+'''
+foo = 20
+for i in range(10):
+	foo -= 1 #Change the - into +
+
+print(foo)
+
+#Fixed:
+
+foo = 20
+for i in range(10):
+	foo += 1
+
+print(foo)
+
