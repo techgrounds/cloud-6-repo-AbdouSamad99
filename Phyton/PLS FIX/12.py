e4dd0b72ff3c55633e4219248d292c2ac119296e
AbdouSamad99 <95620804+AbdouSamad99@users.noreply.github.com>
2022-02-15 11:57:03 +0100

    Create 12.py

---
+++ Phyton/PLS FIX/12.py
@@ -0,0 +1,28 @@
+'''
+The output should be:
+4
+16129
+'''
+def square(x):
+	return x**2
+
+nr = square(2)
+print(nr)
+
+big = square(foo)
+print(big)
+
+foo = 127 #first foo = than big =
+
+#Fixed:
+
+def square(x):
+	return x**2
+
+nr = square(2)
+print(nr)
+
+foo = 127
+big = square(foo)
+print(big)
+
